023208861748a8a1d75bda80ef8b9dccb2eb34cf
Spit <19otari98@gmail.com>
2024-09-19 11:09:02 +0300

    Revert "Add files via upload"
    This reverts commit 20f059d7c2e3ae5d3eb2fb38039ef54d56efbf4e.

--- Core/Spells.lua
+++ Core/Spells.lua
@@ -1,7 +1,7 @@
 
 local AL = AceLibrary("AceLocale-2.2"):new("AtlasLoot");
 
-GetSpellInfoAtlasLootDB = {
+GetSpellInfoVanillaDB = {
 	["enchants"] = {
 		[7418] = {
 			["name"] = "Enchant Bracer - Minor Health",
